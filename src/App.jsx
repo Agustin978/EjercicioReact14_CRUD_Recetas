f0c42b320f4a199cbe50a7e29904e5976ff37cd2
Agustin978 <loboagus10@gmail.com>
2023-06-14 00:28:54 -0300

    Comente el metodo de linkeo para los links de administrador para que de ello se encarguen los elementos RutasAdministrador y RutasProtegidas las cuales se llaman en el caso que en el linkeo se ingrese ../administrador/..

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import Error404 from "./components/views/Error404";
 import Menu from "./components/common/Menu";
 import Footer from "./components/common/Footer";
 import Inicio from "./components/views/Inicio";
-import Administrador from "./components/views/Administrador";
-import CrearReceta from "./components/views/receta/CrearReceta"
+//import Administrador from "./components/views/Administrador";
+//import CrearReceta from "./components/views/receta/CrearReceta"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/views/Login";
+import RutasProtegidas from "./components/routes/RutasProtegidas";
+import RutasAdministrador from "./components/routes/RutasAdministrador";
 
 function App() {
   return (
@@ -14,8 +17,14 @@ function App() {
       <Menu></Menu>
       <Routes>
         <Route exact path="/" element={<Inicio></Inicio>}></Route>
-        <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
-        <Route exact path="/administrador/crear" element={<CrearReceta></CrearReceta>}></Route>
+        {/*<Route exact path="/administrador" element={<Administrador></Administrador>}></Route>*/}
+        {/*<Route exact path="/administrador/crear" element={<CrearReceta></CrearReceta>}></Route>*/}
+        <Route exact path="/login" element={<Login></Login>}></Route>
+        <Route path="/administrador/*" element={
+          <RutasProtegidas>
+            <RutasAdministrador></RutasAdministrador>
+          </RutasProtegidas>
+        }></Route>
         <Route path="*" element={<Error404></Error404>}></Route>
       </Routes>
       <Footer></Footer>
