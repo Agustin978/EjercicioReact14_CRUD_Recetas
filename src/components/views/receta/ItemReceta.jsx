620cfe5dc65e3919d358adc711c664fee973f28e
MauroDS <mauro.diaz.santilli@gmail.com>
2023-06-15 01:03:33 -0300

    Estableciendo una estructura para recetas

--- src/components/views/receta/ItemReceta.jsx
+++ src/components/views/receta/ItemReceta.jsx
@@ -1,18 +1,58 @@
 import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import 'sweetalert2/dist/sweetalert2.css'
+import Swal from "sweetalert2";
+import { consultaBorrarProducto, obtenerProductos } from "../../helpers/queries";
 
 
-const ItemReceta = () => {
+const ItemProducto = ({producto, setProductos}) => {
+
+  const borrarProducto =()=>{
+    Swal.fire({
+      title: '¿Esta seguro de eliminar el producto?',
+      text: "No se puede revertir este paso",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Borrar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+      
+        //aqui tengo que hacer la peticion DELETE
+        consultaBorrarProducto(producto.id).then( (respuesta) =>{
+          if(respuesta.status === 200){
+            Swal.fire(
+              'Producto eliminado',
+              `El producto ${producto.nombreProducto} fue eliminado`,
+              'success'
+            );
+            //actualizar el state producto del componente Administrador
+            obtenerProductos().then((respuesta)=> setProductos(respuesta) )
+            
+          }else{
+            Swal.fire(
+              'Se produjo un error',
+              `Intente realizar esta operacion mas tarde`,
+              'error'
+            )
+          }
+        })
+      }
+    })
+  }
+
    return (
     <tr>
-      {/* <td>{props.producto._id}</td> */}
-      <td>1</td>
-      <td>MOCHACCINO CANELA</td>
-      <td>$1.740,00</td>
-      <td>https://images.pexels.com/photos/6802983/pexels-photo-6802983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1</td>
-      <td>Café</td>
+      <td>{producto.id}</td>
+      <td>{producto.nombreProducto}</td>
+      <td>${producto.precio}</td>
+      <td>{producto.imagen}</td>
+      <td>{producto.categoria}</td>
       <td>
-        <Button className="btn btn-warning">Editar</Button>
-        <Button variant="danger">
+        <Link className="btn btn-warning" to={`/administrador/editar/${producto.id}`}>Editar</Link>
+        <Button variant="danger" onClick={borrarProducto}>
           Borrar
         </Button>
       </td>
@@ -20,4 +60,4 @@ const ItemReceta = () => {
   );
 };
 
-export default ItemReceta;
+export default ItemProducto;
