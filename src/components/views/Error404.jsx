96a529968a5552f66b1e4aa8f013af66fc05f697
MauroDS <mauro.diaz.santilli@gmail.com>
2023-06-15 00:38:54 -0300

    Agregando texto para explicar el error 404

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,19 +1,19 @@
 import { Button } from 'react-bootstrap';
-import error from '../../assets/Error404.gif'
-import "../../App.css"
+import error from '../../assets/Error404.gif';
+import '../../App.css';
 
 const Error404 = () => {
-    return (
-        <section className="mainSection text-center">
-            <article>
-             <img src={error} alt="error 404" className='error404-img'/>
-             <h1 className='error404-texto'>Error 404</h1>
-            </article>
-            <div className='error404-contenido'>
-            <Button variant='danger'>Volver al inicio</Button>
-            </div>
-        </section>
-    );
+  return (
+    <section className="mainSection text-center">
+      <img src={error} alt="error 404" className="error404-img" />
+      <h1 className="error404-texto">Error 404</h1>
+      <p className="error404-texto2">Ups. La página a la que intentas acceder no se encuentra disponible. 
+        Para volver a la página principal, haz clic en el botón de abajo.</p>
+        <div className="error404-contenido">
+        <Button variant="danger">Volver al inicio</Button>
+      </div>
+    </section>
+  );
 };
 
 export default Error404;
