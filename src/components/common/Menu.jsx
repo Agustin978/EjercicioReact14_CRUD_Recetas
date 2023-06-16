673f30506b97cc5597103978667e441b61c2a914
Agustin978 <loboagus10@gmail.com>
2023-06-16 20:59:37 -0300

    Creada una funcion flecha de logout para el cerrado de secion. Ahora si un usuario esta logueado en el navbar se muestra la pestaña de Administrador y se cambia el login por un boton de logout y en caso que no hayan usuarios logueados se muestra el link de login en lugar de los dos anteriores.

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,7 +1,16 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
-import { Link, NavLink } from 'react-router-dom';
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
-const Menu = () => {
+const Menu = ({usuarioLogueado, setUsuarioLogueado}) => {
+  const navigate = useNavigate();
+  const logout = () => 
+  {
+    sessionStorage.removeItem('user');
+    setUsuarioLogueado({});
+    navigate('/');
+  }
+
+
   return (
     <Navbar bg="info" variant="dark" expand="lg">
       <Container>
@@ -10,7 +19,14 @@ const Menu = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <NavLink end className='nav-item nav-link' to={'/'}>Inicio</NavLink>
-            <NavLink end className='nav-item nav-link' to={'/administrador'}>Administrador</NavLink>
+            {
+              usuarioLogueado.email?(
+                <>
+                  <NavLink end className='nav-item nav-link' to={'/administrador'}>Administrador</NavLink>
+                  <Button variant="danger" onClick={logout}>Logout</Button>
+                </>
+              ):<NavLink end className='nav-item nav-link' to={'/login'}>Login</NavLink>
+            }
             <NavLink end className='nav-item nav-link' to={'*'}>Contacto</NavLink>
           </Nav>
         </Navbar.Collapse>
