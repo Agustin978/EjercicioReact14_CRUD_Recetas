454b88eff4fb4f60a37e80e81a03eb60a24e3c97
MauroDS <mauro.diaz.santilli@gmail.com>
2023-06-18 17:27:18 -0300

    Añadiendo mismo tamaño de imagen a las cards y truncado para descripcion

--- src/components/views/receta/CardReceta.jsx
+++ src/components/views/receta/CardReceta.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Col, Card, Button } from "react-bootstrap";
+import "../../../App.css"
 
 const CardReceta = ({ receta }) => {
   return (
     <Col md={4} lg={3} className="mb-3">
-      <Card>
-        <Card.Img variant="top" src={receta.imagen} />
+      <Card className="RecetaCard">
+        <Card.Img className="RecetaCard-img" variant="top" src={receta.imagen} />
         <Card.Body>
           <Card.Title>{receta.nombrePlatillo}</Card.Title>
-          <Card.Text>{receta.descripcion}</Card.Text>
+          <Card.Text className="RecetaCard-desc">{receta.descripcion}</Card.Text>
           <Button variant="primary">Ver detalle</Button>
         </Card.Body>
       </Card>
