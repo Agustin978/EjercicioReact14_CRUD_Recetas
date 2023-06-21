0c6e4284c7a9d9933dd7bb3aaa8d45db3ce045a5
Agustin978 <loboagus10@gmail.com>
2023-06-21 01:41:06 -0300

    Se usa el useEffect para pedir los comentarios almacenados en la API de la receta en la que se encuentra, luego se los muestra en cards

--- src/components/views/ComentariosReceta.jsx
+++ src/components/views/ComentariosReceta.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+import { obtieneComentarios } from "../helpers/queries";
+import Swal from "sweetalert2";
 
-const ComentariosReceta = ({receta}) => {
+const ComentariosReceta = ({id_receta}) => {
+    const [comentarios, setComentarios] = useState([]);
+    useEffect(()=>
+    {
+        obtieneComentarios(id_receta).then(respuesta => 
+            {
+                if(respuesta)
+                {
+                    setComentarios(respuesta);
+                }else
+                {
+                    Swal.fire('Error', 'Hay inconvenientes para conectarse a la base de datos actualmente. Por favor, Intenta nuevamente mas tarde.', 'error');
+                }
+            })
+    },[])
     return (
-        <div>
-            
-        </div>
+        <>
+            {comentarios.map((comentario) =>(
+                <Card key={comentario.id} className="my-2">
+                    <Card.Header className="h6">{comentario.nombreUsuario}</Card.Header>
+                    <Card.Body>
+                        <Card.Text>{comentario.comentario}</Card.Text>
+                    </Card.Body>
+                </Card>
+            ))}
+        </>
     );
 };
 
